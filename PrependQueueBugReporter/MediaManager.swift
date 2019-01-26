513beab1dec0ed62cef19094d2eff387c505d11e
mattmega4 <mattmega4@gmail.com>
2019-01-25 20:04:10 -0800

    created sysdiagnose

--- PrependQueueBugReporter/MediaManager.swift
+++ PrependQueueBugReporter/MediaManager.swift
@@ -7,7 +7,44 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class MediaManager: NSObject {
+
+  static let shared = MediaManager()
+  var playedSongs = [MPMediaItem]()
+  var lockedSongs = [MPMediaItem]()
+
+  func getAllSongs(completion: @escaping (_ songs: [MPMediaItem]?) -> Void) {
+    MPMediaLibrary.requestAuthorization { (status) in
+      if status == .authorized {
+        let query = MPMediaQuery()
+        let mediaTypeMusic = MPMediaType.music
+        let audioFilter = MPMediaPropertyPredicate(value: mediaTypeMusic.rawValue, forProperty: MPMediaItemPropertyMediaType, comparisonType: MPMediaPredicateComparison.equalTo)
+        query.addFilterPredicate(audioFilter)
+        let songs = query.items?.filter({ (item) -> Bool in
+          //          return item.mediaType.rawValue == 1
+          return item.mediaType.rawValue <= MPMediaType.anyAudio.rawValue
+        })
+        completion(songs)
+      } else {
+        completion(nil)
+      }
+    }
+  }
+
+  func getSongsWithCurrentArtistFor(item: MPMediaItem) -> MPMediaQuery {
+    let artistPredicate = MPMediaPropertyPredicate(value: item.artist, forProperty: MPMediaItemPropertyArtist, comparisonType: .contains)
+    let query = MPMediaQuery()
+    query.addFilterPredicate(artistPredicate)
+    return query
+  }
+
+  func removeArtistLockFor(item: MPMediaItem) -> MPMediaQuery {
+    let artistPredicate = MPMediaPropertyPredicate(value: item.artist, forProperty: MPMediaItemPropertyArtist)
+    let query = MPMediaQuery()
+    query.removeFilterPredicate(artistPredicate)
+    return query
+  }
 
 }
--- PrependQueueBugReporter/ViewController.swift
+++ PrependQueueBugReporter/ViewController.swift
@@ -7,14 +7,180 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
+
+  @IBOutlet weak var songLabel: UILabel!
+  @IBOutlet weak var albumLabel: UILabel!
+  @IBOutlet weak var artistLabel: UILabel!
+  @IBOutlet weak var artistLockButton: UIButton!
+  @IBOutlet weak var prevSongButton: UIButton!
+  @IBOutlet weak var playPauseButton: UIButton!
+  @IBOutlet weak var nextSongButton: UIButton!
+
+  let mediaPlayer = MPMusicPlayerController.systemMusicPlayer
+  var newSongs = [MPMediaItem]()
+  var currentSong: MPMediaItem?
+  var audioSession = AVAudioSession.sharedInstance()
+  var artistIsLocked = false
+  var isPlaying = false
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+
+    DispatchQueue.main.async {
+      self.mediaPlayer.beginGeneratingPlaybackNotifications()
+      NotificationCenter.default.addObserver(self, selector: #selector(self.songChanged(_:)), name: NSNotification.Name.MPMusicPlayerControllerNowPlayingItemDidChange, object: self.mediaPlayer)
+
+      MediaManager.shared.getAllSongs { (songs) in
+        guard let theSongs = songs else {
+          return
+        }
+        self.mediaPlayer.nowPlayingItem = nil
+        self.newSongs.removeAll()
+        self.newSongs = theSongs.filter({ (item) -> Bool in
+          return !MediaManager.shared.playedSongs.contains(item)
+        })
+
+        self.mediaPlayer.setQueue(with: MPMediaItemCollection(items: self.newSongs.shuffled()))
+        self.mediaPlayer.shuffleMode = .songs
+        self.mediaPlayer.repeatMode = .none
+      }
+    }
+
+    var canBecomeFirstResponder: Bool { return true }
+    self.becomeFirstResponder()
+    do {
+      try AVAudioSession.sharedInstance().setCategory(.soloAmbient, mode: .default, options: .duckOthers)
+      try AVAudioSession.sharedInstance().setActive(true)
+    } catch {
+      print("Error setting the AVAudioSession:", error.localizedDescription)
+    }
+
+  }
+
+  @objc func songChanged(_ notification: Notification) {
+    DispatchQueue.main.async {
+       self.getCurrentlyPlayedInfo()
+    }
+  }
+
+  func getCurrentlyPlayedInfo() {
+
+    if let songInfo = self.mediaPlayer.nowPlayingItem {
+      self.songLabel.text = songInfo.title ?? ""
+      self.albumLabel.text = songInfo.albumTitle ?? ""
+      self.artistLabel.text = songInfo.artist ?? ""
+
+    }
+  }
+
+  func tappedLockLogic() {
+    if self.artistIsLocked == true {
+      self.artistLockButton.setTitle("Artist Locked", for: .normal)
+    } else {
+      self.artistLockButton.setTitle("Artist Unlocked", for: .normal)
+    }
+  }
+
+  func clearSongInfo() {
+    DispatchQueue.main.async {
+
+      self.songLabel.text = ""
+      self.albumLabel.text = ""
+      self.artistLabel.text = ""
+    }
   }
 
 
+  @IBAction func prevSongButtonTapped(_ sender: UIButton) {
+    mediaPlayer.prepareToPlay(completionHandler: { (error) in
+      DispatchQueue.main.async {
+        self.mediaPlayer.prepareToPlay()
+        self.mediaPlayer.skipToPreviousItem()
+
+      }
+      self.getCurrentlyPlayedInfo()
+    })
+
+  }
+
+  @IBAction func playPauseButtonTapped(_ sender: UIButton) {
+    isPlaying = !isPlaying
+    if self.isPlaying {
+      self.playPauseButton.setTitle("Pause", for: .normal)
+      DispatchQueue.main.async {
+        self.mediaPlayer.prepareToPlay()
+        self.mediaPlayer.play()
+        self.getCurrentlyPlayedInfo()
+      }
+    } else {
+      self.playPauseButton.setTitle("Play", for: .normal)
+      DispatchQueue.main.async {
+        self.mediaPlayer.pause()
+        self.getCurrentlyPlayedInfo()
+      }
+
+    }
+
+
+
+  }
+
+  @IBAction func nextSongButtonTapped(_ sender: UIButton) {
+    mediaPlayer.prepareToPlay(completionHandler: { (error) in
+      DispatchQueue.main.async {
+        //        guard let nowPlaying = self.mediaPlayer.nowPlayingItem else {
+        //          return
+        //        }
+        //        if self.albumIsLocked && MediaManager.shared.hasPlayedAllSongsFromAlbumFor(song: nowPlaying) || self.artistIsLocked && MediaManager.shared.hasPlayedAllSongsFromArtistFor(song: nowPlaying) || self.genreIsLocked && MediaManager.shared.hasPlayedAllSongsFromGenreFor(song: nowPlaying) {
+        //          self.unlockEverythingAndPlay()
+        //        } else {
+        self.mediaPlayer.prepareToPlay()
+        self.mediaPlayer.skipToNextItem()
+        //        }
+
+        self.getCurrentlyPlayedInfo()
+      }
+    })
+  }
+
+  @IBAction func artistLockButtonTapped(_ sender: UIButton) {
+    DispatchQueue.main.async {
+      if let nowPlaying = self.mediaPlayer.nowPlayingItem {
+        if sender.isSelected {
+          sender.isSelected = false
+          self.artistIsLocked = false
+          self.artistLockButton.setTitle("Artist Unlocked", for: .normal)
+          self.tappedLockLogic()
+          let unlockArtist = MediaManager.shared.removeArtistLockFor(item: nowPlaying)
+          if var items = unlockArtist.items?.filter({ (item) -> Bool in
+            return item.mediaType.rawValue <= MPMediaType.anyAudio.rawValue
+          }) {
+            items.shuffle()
+            let descriptor = MPMusicPlayerMediaItemQueueDescriptor(itemCollection: MPMediaItemCollection(items: self.newSongs.shuffled()))
+            self.mediaPlayer.prepend(descriptor)
+            self.getCurrentlyPlayedInfo()
+          }
+          self.tappedLockLogic()
+
+        } else {
+          sender.isSelected = true
+          self.artistIsLocked = true
+          self.artistLockButton.setTitle("Artist Locked", for: .normal)
+          let lockArtist = MediaManager.shared.getSongsWithCurrentArtistFor(item: nowPlaying)
+          if var items = lockArtist.items {
+            items.shuffle()
+            let descriptor = MPMusicPlayerMediaItemQueueDescriptor(itemCollection: MPMediaItemCollection(items: items))
+            self.mediaPlayer.prepend(descriptor)
+          }
+          self.tappedLockLogic()
+        }
+      }
+    }
+  }
+  
 }
 
