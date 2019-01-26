8efdae3c8f789cd602363408930e0129ba6a6ea2
mattmega4 <mattmega4@gmail.com>
2019-01-25 19:25:06 -0800

    first commit

---
+++ PrependQueueBugReporter/MediaManager.swift
@@ -0,0 +1,13 @@
+//
+//  MediaManager.swift
+//  PrependQueueBugReporter
+//
+//  Created by Matthew Howes Singleton on 1/25/19.
+//  Copyright © 2019 Matthew Howes Singleton. All rights reserved.
+//
+
+import UIKit
+
+class MediaManager: NSObject {
+
+}
